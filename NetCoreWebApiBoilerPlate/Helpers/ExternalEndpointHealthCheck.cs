a2cd01e266eedbe952c875a0a3e2f9d10b24985c
Carlos Vaquedano <carlos.vaquedano@acklenavenue.com>
2021-04-17 21:52:04 -0600

    adding HealthCheck for ExternalEndpoint

---
+++ NetCoreWebApiBoilerPlate/Helpers/ExternalEndpointHealthCheck.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetCoreWebApiBoilerPlate.Helpers
+{
+    public class ExternalEndpointHealthCheck : IHealthCheck
+    {
+        private readonly ServiceSetting _serviceSetting;
+        public ExternalEndpointHealthCheck(IOptions<ServiceSetting> option)
+        {
+            _serviceSetting = option.Value;
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            Ping ping = new();
+            var reply = await ping.SendPingAsync(_serviceSetting.OpenWeatherHost);
+            if (reply.Status != IPStatus.Success)
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
--- NetCoreWebApiBoilerPlate/Startup.cs
+++ NetCoreWebApiBoilerPlate/Startup.cs
@@ -71,12 +71,8 @@ namespace NetCoreWebApiBoilerPlate
                 .AddTransientHttpErrorPolicy( builder => builder.WaitAndRetryAsync(10, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))))
                 .AddTransientHttpErrorPolicy( builder => builder.CircuitBreakerAsync(3, TimeSpan.FromSeconds(15)));
 
-
-
-
-
-
-
+            services.AddHealthChecks()
+                .AddCheck<ExternalEndpointHealthCheck>("OpenWeather");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -111,6 +107,7 @@ namespace NetCoreWebApiBoilerPlate
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
